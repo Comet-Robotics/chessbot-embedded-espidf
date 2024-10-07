88b584ed4e483fb64f46b0fe08055532381c5d24
Colin Wong <cwongmath@gmail.com>
2024-10-06 23:50:59 -0500

    Restructure repo

--- src/server/api/managers.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This module creates global singleton instances of the various manager classes.
- */
-
-import { ClientManager } from "./client-manager";
-import { SocketManager } from "./socket-manager";
-
-export const socketManager = new SocketManager({});
-export const clientManager = new ClientManager(socketManager);
