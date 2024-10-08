1e52bbd61271d8c84ee890efee059e82ee2205da
Mason Thomas <Kandleseatwicks@gmail.com>
2024-10-08 04:42:15 -0500

    hardcode config values

--- main/src/config.cpp
+++ main/src/config.cpp
@@ -5,5 +5,23 @@
 #include <chessbot/config.h>
 
 namespace chessbot {
-uint32_t configStore[(size_t)ConfigKey::CONFIG_COUNT] = {};
+uint32_t configStore[(size_t)ConfigKey::CONFIG_COUNT] = {
+    bitcast<uint32_t>((int)33),
+    bitcast<uint32_t>((int)38),
+    bitcast<uint32_t>((int)39),
+    bitcast<uint32_t>((int)40),
+    bitcast<uint32_t>((int)32),
+    bitcast<uint32_t>((int)31),
+    bitcast<uint32_t>((int)18),
+    bitcast<uint32_t>((int)34),
+    bitcast<uint32_t>((int)15),
+    bitcast<uint32_t>((int)0),
+    bitcast<uint32_t>((int)1),
+    bitcast<uint32_t>((int)3),
+    bitcast<uint32_t>((int)5),
+    bitcast<uint32_t>((float)4.375),
+    bitcast<uint32_t>((float)-1.0),
+    bitcast<uint32_t>((float)1.0),
+    bitcast<uint32_t>((float)-12000.0)
+};
 }; // namespace chessbot
